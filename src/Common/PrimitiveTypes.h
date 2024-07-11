ba755e353cacfcc648db5085c30fbb73fb572c78
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-07-11 19:52:48 +0200

    feat: add unsigned and signed integers and signed float, remove the useless background.jpg

---
+++ src/Common/PrimitiveTypes.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+// unsigned integers
+using u64 = uint64_t;
+using u32 = uint32_t;
+using u16 = uint16_t;
+using u8 = u_int8_t;
+
+// signed integers
+using s64 = int64_t;
+using s32 = int32_t;
+using s16 = int16_t;
+using s8 = int8_t;
+
+using f32 = float;
