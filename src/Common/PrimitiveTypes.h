81ed4b2e8c0a792f686c5537a877655fdefbf2ac
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2024-07-11 20:06:49 +0200

    refactor: rename hexadecimal constants

--- src/Common/PrimitiveTypes.h
+++ src/Common/PrimitiveTypes.h
@@ -18,8 +18,8 @@ using s8 = int8_t;
 // defining constants with hexadecimal representations for invalid IDs of different integer types
 // the values are set to represent the maximum possible value for each integer type
 constexpr u64 u64_invalid_id { 0xffff'ffff'ffff'ffffui64 }
-constexpr u32 u64_invalid_id { 0xffff'ffffui32 }
-constexpr u16 u64_invalid_id { 0xffffui16 }
-constexpr u8 u64_invalid_id { 0xffui8 }
+constexpr u32 u32_invalid_id { 0xffff'ffffui32 }
+constexpr u16 u16_invalid_id { 0xffffui16 }
+constexpr u8 u8_invalid_id { 0xffui8 }
 
 using f32 = float;
